b56d2e1f0df51e6abdb167cb88287cc9a8943385
Dev simeon <elebesunuosereme@gmail.com>
2023-09-02 16:32:09 +0100

    added password hide & show change input font size

--- app.js
+++ app.js
@@ -3,13 +3,37 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const  password = document.querySelector('#password');
 const  password2 = document.querySelector('#password2');
+const eye1 = document.querySelector('.e1')
+const eye2 = document.querySelector('.e2')
+
+eye1.addEventListener('click', function(){
+    if(password.type === 'password'){
+        password.type = 'text'
+    }
+    else{
+        password.type = 'password'
+    }
+    eye1.classList.toggle('fa-eye')
+})
+eye2.addEventListener('click', function(){
+    
+    eye2.classList.toggle('fa-eye')
+    if(password2.type === 'password'){
+        password2.type = 'text'
+    }
+    else{
+        password2.type = 'password'
+    }
+
+})
+
 
 //Submiting the form
 form.addEventListener('submit', (e) =>{
-    // e.preventDefault();
-    let load = e.preventDefault()
+    e.preventDefault();
     validateInputs();
-    return load
+  
+
 });
 
 
@@ -43,9 +67,10 @@ const validateInputs = () =>{
     const passwordValue = password.value.trim();
     const password2Value = password2.value.trim();
     
-
+    let validator = true;
     if(usernameValue === ''){
         setError(username,'username required')
+        validator = false;
     }
     else{
         setSuccess(username)
@@ -53,28 +78,41 @@ const validateInputs = () =>{
 
     if(emailValue === ""){
         setError(email,'email required')
+        validator = false;
+
        
     }
     else{
         setSuccess(email)
+        validator = true;
+
     }
     // first password validation
     if(passwordValue === ''){
         setError(password,'username required')
+        validator = false;
+
     } else if(passwordValue.length > 8){
         setError(password, ' Password must be at least 8 character')
+        validator = false;
+
     }
     else{
         setSuccess(password)
+
     };
     //second password validation
     if(password2Value === ''){
         setError(password2,'Please confirm password')
+        validator = false;
+
     } else if(password2Value !== passwordValue){
         setError(password2,"Password dosen't match")
+        validator = false;
     }
     else{
         setSuccess(password2)
     };
+    return validator
     
 }
